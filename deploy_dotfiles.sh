09a012aa034e55d1ebb37b197896f3585bd85ed8
Johan Berglind <johan@berglind.me>
2015-02-08 23:50:46 +0100

    Minor fix to deploy-script

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone git@github.com:johanberglind/dotfiles.git
+git clone https://github.com/johanberglind/dotfiles.git
 echo "dotfiles repo cloned!"
 cd dotfiles
 cp emacs ~/.emacs
